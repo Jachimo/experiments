8d9a31f7a1bbf0565e254ff426b35be8fc2a1cd8
Jachimo <Jachimo@users.noreply.github.com>
2010-01-16 02:50:20 -0500

    Created a little program to play with stdin in Python

---
+++ programming/python/stdintest.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python
+
+import sys
+
+data = sys.stdin
+
+print "-> The first line is:"
+print data.readline()
+
+print "-> The next 10 lines are:"
+i = 0
+while i < 10:
+    print data.readline()
+    i++
+
+sys.exit(0)

3b2dfb4bf90c12b92c780833042a8b25b8a87fa8
Jachimo <Jachimo@users.noreply.github.com>
2010-02-05 00:18:36 -0500

    Made the binaryXOR program more functional (in terms of syntax, not behavior).  Untested.

--- programming/python/binaryXOR.py
+++ programming/python/binaryXOR.py
@@ -7,6 +7,9 @@ import struct
 
 def main():    
     # Open input and output files (if no output, use stdout)
+    if len(sys.argv) < 3:
+        sys.stderr.write("Insufficient arguments. Exiting.\n")
+        return 1
     try:
         file1 = open(sys.argv[1], "rb")
     except IOError:
@@ -26,27 +29,46 @@ def main():
             sys.stderr.write("Could not open specified output file. Exiting.\n")
             return 1
 
-    # Then read through the files, byte by byte, and XOR them:
-    while True:
-        # Read one byte from the first file
-        char1 = file1.read(1)
-        if not char1:
-            break
-        # Read one byte from the second
-        char2 = file2.read(1)
-        if not char2:
-            sys.stderr.write("File 2 ended unexpectedly. Retry with longer file.\n")
-            break
-        # XOR the two bytes together, using struct to convert str -> binary
-        outbyte = struct.unpack('b',char1)[0]^struct.unpack('b',char2)[0]
-        # Write the XORed output to a file, converting back to str
-        outfile.write( struct.pack('b',outbyte) )
+    # Do the work; see below for details
+    outfile.write( fileXOR(file1, file2) )
 
     # Close files, exit without error condition
     file1.close()
     file2.close()
     outfile.close()
     return 0
+
+
+def fileXOR(file1, file2):
+    "Take two file objects and XOR them together. Returns a string."
+
+    outlist = []  # List to store output
+
+    # Then read through the files, byte by byte, and XOR them:
+    while True:
+
+        # Read one byte from the first file
+        char1 = file1.read(1)
+        # Stop when we reach the end of the file
+        if not char1:
+            break
+        
+        # Read one byte from the second
+        char2 = file2.read(1)
+        if not char2:
+            sys.stderr.write("File 2 ended unexpectedly. Retry with longer file.\n")
+            break
+
+        # XOR the two bytes together, using struct to convert str -> binary
+        #  (The python binary XOR operator is "^")
+        outbyte = struct.unpack('b',char1)[0]^struct.unpack('b',char2)[0]
+        
+        # Return the XORed output to a list, converting back to str
+        outlist.append( struct.pack('b',outbyte) )
+
+    # Collapse the list down to a string
+    return ''.join(outlist)
+
 
     
 # Run the program when executed, starting from main()    

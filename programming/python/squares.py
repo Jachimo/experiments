255fc79e8afc79e2c924e483fe58da10568f5d70
Jachimo <Jachimo@users.noreply.github.com>
2010-02-08 15:09:34 -0500

    Worked on python

--- programming/python/squares.py
+++ programming/python/squares.py
@@ -2,22 +2,24 @@
 
 import sys
 
-def squares():
-    # Prompt users for a number and a number of iterations
-    #  Then construct a list of squares of that number.
-    
-    a = int(raw_input("Enter an integer: "))
-    max = int(raw_input("Enter number of iterations: "))
+def squares(a, stop):
+    """Take an integer A and an integer number of iterations S,
+    and construct a list of squares A**n, where n is from 1 to S.
+
+    Returns a list."""
 
     L = []
     i = 1
-    while i < max:
+    while i <= stop:
         L.append(a**i)
         i = i+1
 
     return L
 
-# For test purposes, print it:
-for item in squares():
-    print item
+# For test purposes, work interactively:
+if __name__ == "__main__":
+    start = int(raw_input("Enter an integer: "))
+    iterations = int(raw_input("Enter number of iterations: "))
+    for item in squares(start, iterations):
+        print item
 

db51096ae53ceea0aae04c954392f50a8689b26d
Jachimo <jachimo@noreply.github.com>
2025-10-20 01:45:37 -0400

    Added set_color_xattr() although this doesn't work on some SMB shares

--- apple_metadata/src/samba_labels/cli.py
+++ apple_metadata/src/samba_labels/cli.py
@@ -3,8 +3,11 @@
 import sys
 import os
 import logging
+import xattr  # see https://github.com/iustin/pyxattr
+
 from samba_labels import __version__
 from samba_labels.processor import AppleDoubleMetadata
+
 
 def dump_file(args=sys.argv, loglev=logging.DEBUG) -> None:
     """ Dump contents of AppleDouble file """
@@ -21,13 +24,40 @@ def print_finder_color(args=sys.argv, loglev=logging.WARNING) -> None:
     if len(args) != 2:
         raise ValueError(f"Wrong number of arguments: {args}")
     inpath: str = args[1]
+    # Use get_finder_color() below
+    print(f"{inpath.split(os.path.sep)[-1]}: {get_finder_color(inpath, loglev)}")
 
+
+def get_finder_color(inpath, loglev=logging.WARNING) -> str:
+    """ Get the Finder 'Label' color and return it as a string """
     md: AppleDoubleMetadata = AppleDoubleMetadata(inpath, loglev)
     try:
-        print(f"{inpath.split(os.path.sep)[-1]}: {md.color.name}")
+        return md.color.name
     except:
         raise ValueError(f"Couldn't read Finder color flags in {inpath}")
-        
+
+
+def print_xattrs(args=sys.argv, loglev=logging.DEBUG) -> None:
+    if len(args) != 2:
+        raise ValueError(f"Wrong number of arguments: {args}")
+    inpath: str = args[1]
+
+    xlist: list = xattr.listxattr(inpath)
+    for xa in xlist:
+        print(f"{xa.decode('utf-8')} : {xattr.getxattr(inpath, xa).decode('utf-8')}")
+
+
+def set_color_xattr(args=sys.argv, loglev=logging.DEBUG) -> None:
+    """ Sets the extended attribute user.color to the Finder label color from the AppleDouble metadata.
+        Note that this depends on the underlying filesystem supporting extended attributes. """
+    if len(args) != 2:
+        raise ValueError(f"Wrong number of arguments: {args}")
+    inpath: str = args[1]
+
+    md: AppleDoubleMetadata = AppleDoubleMetadata(inpath, loglev)
+
+    xattr.setxattr(inpath, "user.color", md.color.name)
+
 
 def parse_with_kaitai(args=sys.argv) -> None:
     """ Use the Kaitai-generated Python library to parse an AppleDouble file. """

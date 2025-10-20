8636940806ceb39e8965f1f9c4285e1572a92fd5
Jachimo <jachimo@noreply.github.com>
2025-10-19 22:51:28 -0400

    Readability

--- apple_metadata/src/samba_labels/cli.py
+++ apple_metadata/src/samba_labels/cli.py
@@ -6,16 +6,30 @@ from samba_labels.processor import AppleDoubleMetadata
 
 def dump_file(args=sys.argv) -> int:
     """ Dump contents of AppleDouble file """
-    if len(args) == 2:
-        inpath = args[1]
-    else:
-        print("Wrong number of arguments")
-        return 1
+    if len(args) != 2: 
+        raise ValueError("Wrong number of arguments")
+    inpath = args[1]
     
-    adm = AppleDoubleMetadata(inpath)
-    print(adm.dump())
-
+    md = AppleDoubleMetadata(inpath)
+    print(md.dump())
+    
     return 0
+
+
+def read_finder_color(args=sys.argv) -> int:
+    """ Print the decimal representation of the 3-bit Label flags """
+    if len(args) != 2:
+        raise ValueError("Wrong number of arguments")
+    inpath = args[1]
+
+    md = AppleDoubleMetadata(inpath)
+    try:
+        finderinfo: AppleDoubleMetadata.Entry = md.entries[9]["obj"]
+        print(f"Color flags set to: {finderinfo.finder_colorval}")
+    except:
+        raise ValueError(f"Couldn't read Finder color flags in {inpath}")
+        
+
 
 def parse_with_kaitai(args=sys.argv) -> int:
     """ Use the Kaitai-generated Python library to parse an AppleDouble file. """
